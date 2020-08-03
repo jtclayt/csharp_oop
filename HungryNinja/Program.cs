1d54ba2b15212af0a0b04999785cfffa04021923
Justin Clayton <jt.clayton92@yahoo.com>
2020-08-03 14:03:29 -0700

    Complete Hungry Ninja assignment

---
+++ HungryNinja/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HungryNinja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Buffet buffet = new Buffet();
+            Ninja ninja = new Ninja();
+            while (!ninja.IsFull())
+            {
+                ninja.Eat(buffet.Serve());
+            }
+            ninja.Eat(buffet.Serve());
+        }
+    }
+}

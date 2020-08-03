c7a74af625576409bbcd9ec023c9c054e41129ae
Justin Clayton <jt.clayton92@yahoo.com>
2020-08-03 08:36:44 -0700

    Initial commit.

---
+++ Human/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Human
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+    class Human
+    {
+        private string _name;
+        private int _strength;
+        private int _intelligence;
+        private int _dexterity;
+        private int _health;
+    }
+}

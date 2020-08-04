4032f77bdb39f0fa1cc1d3ee1cc690a46b5bf6fd
Justin Clayton <jt.clayton92@yahoo.com>
2020-08-04 14:39:04 -0700

    Add function for showing total items consumed and display top 2 most consumed items.

--- HungryNinja/Program.cs
+++ HungryNinja/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HungryNinja
 {
@@ -7,18 +8,69 @@ namespace HungryNinja
         static void Main(string[] args)
         {
             Buffet buffet = new Buffet();
-            SweetTooth st = new SweetTooth();
-            SpiceHound sh = new SpiceHound();
-            while (!st.IsFull())
+            List<Ninja> ninjas = new List<Ninja>();
+            ninjas.Add(new SweetTooth());
+            ninjas.Add(new SweetTooth());
+            ninjas.Add(new SpiceHound());
+
+            foreach (var ninja in ninjas)
             {
-                st.Eat(buffet.Serve());
+                while (!ninja.IsFull())
+                {
+                    ninja.Eat(buffet.Serve());
+                }
+                ninja.Eat(buffet.Serve());
+                Console.WriteLine("-------------------------------------------------------------");
             }
-            st.Eat(buffet.Serve());
-            while (!sh.IsFull())
+
+            PrintStats(ninjas);
+        }
+
+        public static void PrintStats(List<Ninja> ninjas)
+        {
+            Dictionary<string, int> counts = new Dictionary<string, int>();
+            int totalConsumed = 0;
+            string topFood = null;
+            int topCount = 0;
+            string secFood = null;
+            int secCount = 0;
+
+            foreach (var ninja in ninjas)
             {
-                sh.Eat(buffet.Serve());
+                totalConsumed += ninja.FoodHistory.Count;
+                foreach (var food in ninja.FoodHistory)
+                {
+                    if (counts.ContainsKey(food.Name))
+                    {
+                        counts[food.Name]++;
+                    }
+                    else{
+                        counts.Add(food.Name, 1);
+                    }
+                }
             }
-            sh.Eat(buffet.Serve());
+
+            foreach (var item in counts)
+            {
+                if (item.Value > topCount)
+                {
+                    if (topFood != null)
+                    {
+                        secFood = topFood;
+                        secCount = topCount;
+                    }
+                    topFood = item.Key;
+                    topCount = item.Value;
+                }
+                else if (item.Value > secCount)
+                {
+                    secFood = item.Key;
+                    secCount =  item.Value;
+                }
+            }
+            Console.WriteLine($"Ninjas ate a total of {totalConsumed} items.");
+            Console.WriteLine($"Most eaten item was {topFood} consumed {topCount} times.");
+            Console.WriteLine($"Next most eaten item was {secFood} consumed {secCount} times.");
         }
     }
 }
